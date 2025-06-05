199ab1d01f37bf84c4b3bef037f591bab42786e4
Everton-Fe <s.everton03@escola.pr.gov.br>
2025-06-05 12:40:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'o que é um verbo?',
+    'verbo é uma classe de palavras'
+)
+
+criaCartao(
+    'História',
+    'Qual ano os dinossauros foram extintos?',
+    'aproximadamente 66 milhões de anos'
+)
+
+criaCartao(
+    'Educação física',
+    'Quando foi criado o vôlei?',
+    'O vôlei foi criado em 1895'
+)
+
+criaCartao(
+    'Lingua portuguesa',
+    'o que é um verbo?',
+    'verbo é uma classe de palavras'
+)
+
+criaCartao(
+    'Educação financeira',
+    'Qual o valor do Dólar no Brasil?',
+    'O Dólar vale 5,60 reais no Brasil'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é fatos sociais?',
+    'O modo de agir, pensar e sentir'
+)
